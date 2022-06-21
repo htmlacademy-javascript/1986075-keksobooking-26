3a5fab71182b0b7694f8277bd7abe43cf42928c1
Pavel Kiselev <95882537+pkis6699@users.noreply.github.com>
2022-06-21 21:34:45 +0300

    исправление ошибок linter

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-"use strict"
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 function getRandomIntInclusive(min, max, numbers) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    if (min >= 0 && max >= 0) {
-      let result = (Math.random() * (max - min + 1));
-    return result.toFixed(numbers)
-    }else{
-      console.log('Число меньше 0');
-    }}
-    console.log(getRandomIntInclusive(0.6, 9.2, 4));
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min >= 0 && max >= 0) {
+    const result = (Math.random() * (max - min + 1));
+    return result.toFixed(numbers);
+  }else{
+    // console.log('Число меньше 0');
+  }}
+
+getRandomIntInclusive(0.6, 9.2, 4);
