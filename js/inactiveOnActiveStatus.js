f9eabdf8df58a25334c033239f59c191671264b3
PavelKis <pavelk364@gmail.com>
2022-07-30 21:18:03 +0300

    edit_01

--- js/inactiveOnActiveStatus.js
+++ js/inactiveOnActiveStatus.js
@@ -1,20 +1,20 @@
 
 function toInactive() {
-    let adForm = document.querySelector('.ad-form');
+    const adForm = document.querySelector('.ad-form');
     adForm.classList.add('.ad-form--disabled');
-    let parentsForm = adForm.querySelectorAll('fieldset');
+    const parentsForm = adForm.querySelectorAll('fieldset');
     parentsForm.forEach((element) => {
         element.setAttribute("disabled", "disabled");
     });
     
 
-    let mapFiltersForm = document.querySelector('.map__filters');
+    const mapFiltersForm = document.querySelector('.map__filters');
     mapFiltersForm.querySelectorAll('select');
-    mapFiltersForm.querySelector('fieldset').setAttribute("disabled", "disabled");
-    let disMapFilters = mapFiltersForm.querySelectorAll('select');
+    mapFiltersForm.querySelector('fieldset').setAttribute("disabled", "");
+    const disMapFilters = mapFiltersForm.querySelectorAll('select');
 
     disMapFilters.forEach((element) => {
-        element.setAttribute("disabled", "disabled");
+        element.setAttribute("disabled", "");
     });
 
 };
@@ -23,18 +23,18 @@ toInactive();
 
 
 function toactive() {
-    let adForm = document.querySelector('.ad-form');
+    const adForm = document.querySelector('.ad-form');
     adForm.classList.remove('.ad-form--disabled');
-    let parentsForm = adForm.querySelectorAll('fieldset');
+    const parentsForm = adForm.querySelectorAll('fieldset');
     parentsForm.forEach((element) => {
         element.setAttribute("disabled", "");
     });
     
 
-    let mapFiltersForm = document.querySelector('.map__filters');
+    const mapFiltersForm = document.querySelector('.map__filters');
     mapFiltersForm.querySelectorAll('select');
     mapFiltersForm.querySelector('fieldset').setAttribute("disabled", "");
-    let disMapFilters = mapFiltersForm.querySelectorAll('select');
+    const disMapFilters = mapFiltersForm.querySelectorAll('select');
 
     disMapFilters.forEach((element) => {
         element.setAttribute("disabled", "");
